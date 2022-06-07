719e85500089bc65f545292c062202447e52753c
Paula-Carlech <89168944+Paula-Carlech@users.noreply.github.com>
2022-06-07 12:20:08 -0300

    Update

--- Atividade06_06/src/Atividade01/Engenheiro.java
+++ Atividade06_06/src/Atividade01/Engenheiro.java
@@ -3,19 +3,19 @@ public class Engenheiro extends Empregado {
     
     private String formacao;
     
-    public Engenheiro(int m, String n, String f ){
-        this.formacao = formacao;
-    }
-    
-    public void gerenciaobra(){
-        System.out.println("Gerenciando obra...");
-    }
-
     public String getFormacao() {
         return formacao;
     }
 
     public void setFormacao(String formacao) {
         this.formacao = formacao;
+    }
+    
+    public Engenheiro(int m, String n, String f ){
+        this.formacao = formacao;
+    }
+    
+    public void gerenciaobra(){
+        System.out.println("Gerenciando obra...");
     }
 }
--- Atividade06_06/src/Atividade01/Secretaria.java
+++ Atividade06_06/src/Atividade01/Secretaria.java
@@ -3,19 +3,19 @@ public class Secretaria extends Empregado{
     
     private String funcao;
     
-    public Secretaria(int m, String n, String f){
-        this.funcao = funcao;
-    }
-    
-    public void imprimirdocumentos(){
-        System.out.println("Imprimindo documentos...");
-    }
-
     public String getFuncao() {
         return funcao;
     }
 
     public void setFuncao(String funcao) {
         this.funcao = funcao;
+    }
+    
+    public Secretaria(int m, String n, String f){
+        this.funcao = funcao;
+    }
+    
+    public void imprimirdocumentos(){
+        System.out.println("Imprimindo documentos...");
     }
 }
